ee2d5da268720d2cb0a46551c4e0b29798de7cc0
Dzhavat Ushev <dzhavatushev@gmail.com>
2019-08-16 23:23:46 +0200

    Code the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,63 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as data from './properties.json';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+interface Property {
+	syntax: string;
+	media: string | string[];
+	inherited: boolean;
+	animationType: string | string[];
+	percentages: string | string[];
+	groups: string[];
+	initial: string | string[];
+	appliesto: string;
+	computed: string | string[];
+	order: string;
+	status: string;
+	mdn_url?: string;
+}
+
+const properties: { [key: string]: Property } = data;
+
 export function activate(context: vscode.ExtensionContext) {
+	const hoverProvider: vscode.HoverProvider = {
+		provideHover(doc, pos, token): vscode.ProviderResult<vscode.Hover> {
+			const range = doc.getWordRangeAtPosition(pos, /[a-z\-]+:/ig);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "css-to-mdn" is now active!');
+			if (range === undefined) {
+				return;
+			}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+			const mdnUrl = getUrl(doc.getText(range));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+			if (mdnUrl === undefined) {
+				return;
+			}
+
+			return new vscode.Hover(getText(mdnUrl));
+		}
+	};
+
+	let disposable = vscode.languages.registerHoverProvider(
+		[
+			{ scheme: 'file', language: 'css' },
+			{ scheme: 'file', language: 'less' },
+			{ scheme: 'file', language: 'sass' },
+			{ scheme: 'file', language: 'scss' }
+		],
+		hoverProvider
+	);
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+function getUrl(word: string): string | undefined {
+	return properties[word.substring(0, word.length - 1)].mdn_url;
+}
+
+function getText(url: string): vscode.MarkdownString {
+	const text = `[Read more on MDN](${url})`;
+
+	return new vscode.MarkdownString(text);
+}
+
 export function deactivate() {}
